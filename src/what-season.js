cfda0831c27d82ffb85431ac6f54df5e3fa59757
Yulia Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2020-02-21 14:31:15 +0300

    feat: add what-season.js implementation

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,25 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (!date) return 'Unable to determine the time of year!';
+    if (date.hasOwnProperty('getMonth')) throw Error('Invalid argument');
+
+    let month = date.getMonth();
+    switch (month) {
+        case 11:
+        case 0:
+        case 1:
+            return 'winter';
+        case 2:
+        case 3:
+        case 4:
+            return 'spring';
+        case 5:
+        case 6:
+        case 7:
+            return 'summer';
+        case 8:
+        case 9:
+        case 10:
+            return 'autumn';
+    }
+    return false;
 };
