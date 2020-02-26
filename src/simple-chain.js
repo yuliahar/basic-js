4998f51d0ff44dada1f0129de74d3a6a56c2c14c
Yulia Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2020-02-26 21:14:32 +0300

    feat: add simple-chain.js implementation

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,38 @@
 const chainMaker = {
+  array: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.array.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (value === undefined) value = ' ';
+    this.array.push(value);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position === 'number' && position > 0 && position <= this.array.length) {
+      this.array.splice(position - 1, 1);
+      return this;
+    }
+    else {
+      this.array = [];
+      throw Error('Invalid position');
+    }
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.array.reverse()
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let string = this.toString();
+    this.array = [];
+    return string;
+  },
+  toString() {
+    let string = '( ' + this.array[0] + ' )';
+    for (let i = 1; i < this.array.length; i++) {
+      string += '~~( ' + this.array[i] + ' )';
+    }
+    return string;
   }
 };
 
