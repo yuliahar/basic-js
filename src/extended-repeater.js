cf4f04d0afe5f2e16b9dcb6d207f514090006010
Yulia Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2020-02-27 23:56:51 +0300

    feat: add extended-repeater.js implementation

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,23 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let result = '';
+    let times = options.hasOwnProperty('repeatTimes') ? options['repeatTimes'] : 1;
+    let separator = options.hasOwnProperty('separator') ? options['separator'] : "+";
+    let addition = options.hasOwnProperty('addition') ? options['addition'] : '';
+    let additionRepeatTimes = options.hasOwnProperty('additionRepeatTimes') ? options['additionRepeatTimes'] : 0;
+    let additionalSeparator = options.hasOwnProperty('additionSeparator') ? options['additionSeparator'] : '';
+    let additionalString = repeatString(addition, additionRepeatTimes, additionalSeparator) ;
+    separator = additionalString + separator;
+
+    result = repeatString(str, times, separator);
+    if (options.hasOwnProperty('addition')) result += additionalString;
+    return result;
 };
-  
+
+function repeatString(str, times, connector) {
+    let result = str;
+    while (times > 1) {
+        result = result + connector + str;
+        times--;
+    }
+    return result;
+};
