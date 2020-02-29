5489266fca044c91d58453576e5bcffb51e013cb
Yulia Harbacheuskaya <yulia.harbacheuskaya@gmail.com>
2020-02-29 12:23:16 +0300

    feat: add vigenere-cipher.js implementation

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,62 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    typeOfMachine = true;
+    alphabet = ("abcdefghijklmnopqrstuvwxyz").toUpperCase().split("");
+
+    constructor(type = true) {
+        this.typeOfMachine = type;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        let result = [];
+        if (!message || !key) throw new Error('Message and key parameters are mandatory.');
+        key = this.transformKeyString(message, key);
+        let keyArray = key.toUpperCase().split("");
+        let iter = 0;
+        message.toUpperCase().split("").forEach((element, i) => {
+            let indexMessageChar = this.alphabet.indexOf(element);
+            if (indexMessageChar < 0) {
+                result.push(element);
+            }
+            else {
+                let indexKeyChar = this.alphabet.indexOf(keyArray[iter]);
+                result.push(this.alphabet[(indexMessageChar + indexKeyChar) % this.alphabet.length]);
+                iter++;
+            }
+        });
+        if (!this.typeOfMachine) return result.reverse().join('');
+        return result.join('');
+    }
+
+    transformKeyString(message, key) {
+        let messageLen = message.length;
+        let keyLen = key.length;
+        let times = message.length / keyLen + 1;
+        key = key.repeat(times).substr(0, messageLen);
+        return key;
+    }
+
+    decrypt(message, key) {
+        let result = [];
+        if (!message || !key) throw new Error('Message and key parameters are mandatory.');
+        key = this.transformKeyString(message, key);
+        let keyArray = key.toUpperCase().split("");
+        let iter = 0;
+        message.toUpperCase().split("").forEach((element, i) => {
+            let indexMessageChar;
+            indexMessageChar = this.alphabet.indexOf(element);
+            if (indexMessageChar < 0) {
+                result.push(element);
+            }
+            else {
+                let indexKeyChar = this.alphabet.indexOf(keyArray[iter]);
+                let temp = (indexMessageChar - indexKeyChar) % this.alphabet.length;
+                if (temp < 0) temp += this.alphabet.length;
+                result.push(this.alphabet[temp]);
+                iter++;
+            }
+        });
+        if (!this.typeOfMachine) return result.reverse().join('');
+        return result.join('');
     }
 }
 
